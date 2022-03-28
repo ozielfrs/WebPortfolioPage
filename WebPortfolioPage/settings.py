02edf1eea619e55d177fc1f35cc2d68a2ace7c3c
Oziel Ferreira <67485143+o-ziel@users.noreply.github.com>
2022-03-28 19:04:36 -0300

    #012 - Added postgre database

--- WebPortfolioPage/settings.py
+++ WebPortfolioPage/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'WebPortfolioPage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd3tdmh2ve86m5o',
+        'HOST': 'ec2-52-21-136-176.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'ihvsjmjcnbolfc',
+        'PASSWORD': '899e6399daee0e940a801419e163a3333bd7ea545965d8a10b40be59a815baa4',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
