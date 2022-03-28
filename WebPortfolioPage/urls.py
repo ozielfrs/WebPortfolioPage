118eabbd97678d640a74911c4f8a434bb0ba3c82
Oziel Ferreira <67485143+o-ziel@users.noreply.github.com>
2022-03-28 11:17:12 -0300

    #005 - Update System OK

--- WebPortfolioPage/urls.py
+++ WebPortfolioPage/urls.py
@@ -14,17 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.views.static import serve
-from django.conf.urls import url
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('adm', admin.site.urls),
     path('', include("Portfolio.urls")),
 
-    url(r'^media/(?P<path>.*)$', serve,
+    re_path(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
+    re_path(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
 ]
